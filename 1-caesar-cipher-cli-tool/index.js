4687dcf65d1bfabbc4730562e215d8f9e48b13c3
VukZh <vukfx@inbox.ru>
2020-03-29 17:30:34 +0600

    feat: console input-output streams and combination with fs streams

--- 1-caesar-cipher-cli-tool/index.js
+++ 1-caesar-cipher-cli-tool/index.js
@@ -6,15 +6,29 @@ const path = require('path');
 const args = require('./args');
 const CaesarCipherTransform = require('./transform');
 
-// const myReadableFileStream = fs.createReadStream (path.join (path.dirname(__filename), args.i), {encoding: 'utf-8'});
-const myReadableFileStream = fs.createReadStream (path.join (path.dirname(__filename), args.i));
+let myReadableFileStream;
+let myWritableFileStream;
 
+// const myReadableFileStream = fs.createReadStream (path.join (path.dirname(__filename), args.i), {encoding: 'utf-8'});
+if (args.i) myReadableFileStream = fs.createReadStream (path.join (path.dirname(__filename), args.i));
+if (args.o) myWritableFileStream = fs.createWriteStream (path.join (path.dirname(__filename), args.o));
 
 // const myWritableFileStream = fs.createWriteStream (path.join (path.dirname(__filename), args.o), {'flags': 'a', encoding: 'utf-8'});
-const myWritableFileStream = fs.createWriteStream (path.join (path.dirname(__filename), args.o));
+
+
+const stdInReadableStream = process.stdin;
+const stdOutWritableStream = process.stdout;
+
+const inputStream = (args.i) ? myReadableFileStream : stdInReadableStream;
+const outputStream = (args.o) ? myWritableFileStream : stdOutWritableStream;
 
 const myTransformStream = new CaesarCipherTransform (args.s, args.a);
 
-myReadableFileStream.pipe(myTransformStream).pipe(myWritableFileStream)
-// myReadableFileStream.pipe(myWritableFileStream)
+
+inputStream.pipe(myTransformStream).pipe(outputStream)
+
+
+// myReadableFileStream.pipe(myTransformStream).pipe(myWritableFileStream)
+
+// stdInReadableStream.pipe(myTransformStream).pipe(stdOutWritableStream)
 
